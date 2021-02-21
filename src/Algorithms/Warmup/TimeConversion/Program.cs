3aa8e9765f4108e7784898cf0d757d5fbdee123e
Aaron Jaeger <aaron.m.jaeger@outlook.com>
2021-02-21 13:25:52 -0600

    Added TimeConversion project.

---
+++ src/Algorithms/Warmup/TimeConversion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        /*
+         * Complete the timeConversion function below.
+         */
+        static string TimeConversion(string s)
+        {
+            var time = DateTime.Parse(s);
+
+            var result = time.ToString("HH:mm:ss");
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = TimeConversion(s);
+
+            tw.WriteLine(result);
+
+            tw.Flush();
+            tw.Close();
+        }
+    }
+}
