95e1fbd2f8c674fcc7e48569354a50967c1a2175
Aaron Jaeger <aaron.m.jaeger@outlook.com>
2021-02-21 12:54:27 -0600

    Added MiniMaxSum project.

---
+++ src/Algorithms/Warmup/MiniMaxSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniMaxSum
+{
+    class Program
+    {
+        // Complete the miniMaxSum function below.
+        static void MiniMaxSum(int[] arr)
+        {
+            var sums = new List<long>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var shortenedList = arr.ToList();
+
+                shortenedList.RemoveAt(i);
+
+                var sum = shortenedList.AsEnumerable()
+                    .Select(x => (long)x)
+                    .Aggregate((number, next) => number + next);
+
+                sums.Add(sum);
+            }
+
+            Console.WriteLine($"{sums.Min()} {sums.Max()}");
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            MiniMaxSum(arr);
+        }
+    }
+}
