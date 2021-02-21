f3afe782a0f25f91548a8681363e6d04eff08882
Aaron Jaeger <aaron.m.jaeger@outlook.com>
2021-02-20 21:10:10 -0600

    Added SimpleArraySum project.

---
+++ src/Algorithms/Warmup/SimpleArraySum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        /*
+        * Complete the simpleArraySum function below.
+        */
+        static int SimpleArraySum(int[] ar)
+        {
+            var sum = ar.AsEnumerable()
+                .Aggregate((number, next) => number + next);
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            int result = SimpleArraySum(ar);
+
+            Console.WriteLine(result);
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
