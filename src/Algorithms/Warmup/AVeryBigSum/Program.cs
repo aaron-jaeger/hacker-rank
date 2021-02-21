314dc000d90377d1481e4432a141f584ab66954d
Aaron Jaeger <aaron.m.jaeger@outlook.com>
2021-02-20 21:50:11 -0600

    Added AVeryBigSum project.

---
+++ src/Algorithms/Warmup/AVeryBigSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+        // Complete the aVeryBigSum function below.
+        static long AVeryBigSum(long[] ar)
+        {
+            var sum = ar.AsEnumerable()
+                .Aggregate((number, next) => number + next);
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp))
+            ;
+            long result = AVeryBigSum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
