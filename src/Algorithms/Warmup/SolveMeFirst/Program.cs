59201239fabd90a9dff424a9b65aac552a10e8bd
Aaron Jaeger <aaron.m.jaeger@outlook.com>
2021-02-20 20:43:32 -0600

    Added SolveMeFirst project.

---
+++ src/Algorithms/Warmup/SolveMeFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SolveMeFirst
+{
+    class Program
+    {
+        static int SolveMeFirst(int a, int b)
+        {
+            // Hint: Type return a+b; below  
+            return a + b;
+        }
+
+        static void Main(String[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = SolveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+        }
+    }
+}
